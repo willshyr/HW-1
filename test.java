96123a2c39caf34df34930dd1deb7ca040f26244
willshyr <williamshyr@Williams-MacBook-Pro.local>
2016-02-14 19:21:49 -0500

    made changes

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         CList<Integer> x= new CList<Integer>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 9; i++) {
             x.insert2(i);
         }
         System.out.println(x);
